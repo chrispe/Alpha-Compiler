56326b5ab3c05f58b97ce6e75b81d03221001bee
chrispe92 <petrop@csd.uoc.gr>
2013-03-16 16:22:46 +0200

    Made some updates...

--- phase1/token.h
+++ phase1/token.h
@@ -2,35 +2,16 @@
     This is an interface used for the
     functionality of tokens.
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-char *token_type_str[] = {
-        "KEYWORD_IF", "KEYWORD_ELSE", "KEYWORD_WHILE", "KEYWORD_FOR", "KEYWORD_FUNCTION",
-        "KEYWORD_RETURN", "KEYWORD_BREAK", "KEYWORD_CONTINUE", "KEYWORD_AND", "KEYWORD_NOT",
-        "KEYWORD_OR", "KEYWORD_LOCAL", "KEYWORD_TRUE", "KEYWORD_FALSE", "KEYWORD_NIL", "OP_EQUAL",
-        "OP_PLUS", "OP_MINUS", "OP_ASTERISK", "OP_SLASH", "OP_PERCENT", 
-        "OP_EQ", "OP_NEQ", "OP_INC", "OP_DEC", "OP_GT", "OP_LT", "OP_EGT",
-        "OP_ELT", "INTEGER", "REAL", "STRING", "PUN_BRACE_L", "PUN_BRACE_R",
-        "PUN_BRACKET_L", "PUN_BRACKET_R", "PUN_PAREN_L", "PUN_PAREN_R",
-        "PUN_SCOLON", "PUN_COMMA", "PUN_COLON", "PUN_DCOLON", "PUN_DOT",
-        "PUN_DDOT", "IDENTIFIER", "SINGLE_COMMENT", "MCOMMENT"
-    };
-    
-    char *key_str[] = {
-        "if", "else", "while", "for", "function",
-        "return", "break", "continue", "and", "not",
-        "or", "local", "true", "false", "nil"
-    };
+extern   char *token_type_str[];
+extern   char *key_str[];
+extern  char *op_str[];
+extern   char *pun_str[];
 
-    char *op_str[] = {
-        "=", "+", "-", "*", "/", "%", "==", "!=",
-        "++", "--", ">", "<", "=>", "<="
-    };
-
-    char *pun_str[] = {
-        "{", "}", "[", "]", "(", ")", ";", ",", ":", "::", ".", ".."
-    };
-
-    typedef enum {
+typedef enum {
         KEYWORD_IF,
         KEYWORD_ELSE,
         KEYWORD_WHILE,
@@ -78,11 +59,17 @@ char *token_type_str[] = {
         IDENTIFIER,
         SINGLE_COMMENT,
         MCOMMENT
-    } token_type_t;
+} token_type_t;
         
-    typedef struct alpha_token {
-        unsigned int line;
-        token_type_t type;
-        void        *content;
-        struct alpha_token *next;
-    } alpha_token_t;
+typedef struct alpha_token {
+    unsigned int line;
+    token_type_t type;
+    void        *content;
+    struct alpha_token *next;
+} alpha_token_t;
+    
+void replace(char *, char *, char);
+    
+void addToken(alpha_token_t **, alpha_token_t **, unsigned int, token_type_t, void *);
+
+void printTokens(alpha_token_t *);
